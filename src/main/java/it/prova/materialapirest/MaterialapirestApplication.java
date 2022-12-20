e9d0def4759e5347ea047468c6c72ff94258f3fd
lsaija <lollosaija@gmail.com>
2022-12-20 22:07:55 +0100

    init

---
+++ src/main/java/it/prova/materialapirest/MaterialapirestApplication.java
@@ -0,0 +1,20 @@
+package it.prova.materialapirest;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialapirestApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialapirestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
